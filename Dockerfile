34d5024e77ebe3abe672a97e439c13744ff36f27
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2024-02-26 12:18:06 +0530

    just

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM public.ecr.aws/lambda/python:3.11
+
+
+
+COPY . .
+
+# Create a directory for data
+RUN mkdir /tmp/data
+
+
+# Set the TRANSFORMERS_CACHE environment variable
+ENV TRANSFORMERS_CACHE "/tmp/data"
+ENV LIBROSA_CACHE_DIR "/tmp"
+ENV NUMBA_CACHE_DIR "/tmp"
+
+COPY ffmpeg '/usr/share/'
+
+COPY Tool '/tmp'
+
+
+
+# Copy requirements.txt and install the required packages
+#COPY requirements.txt .
+RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
+
+#RUN python -m nltk.downloader punkt
+
+# Copy app.py
+COPY app.py ${LAMBDA_TASK_ROOT}
+
+# Set the CMD for the Lambda function
+CMD [ "app.handler" ]
---
+++ app.py
@@ -0,0 +1,47 @@
+import whisper
+import ssl
+import subprocess
+from fastapi import FastAPI,  HTTPException
+from mangum import Mangum
+import os
+import tempfile
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+video_url = 'https://d8cele0fjkppb.cloudfront.net/ivs/v1/624618927537/sgo1MOouPvYg/2023/11/30/5/20/HN1McmrcTVMY/media/hls/master.m3u8'
+
+os.environ["TRANSFORMERS_CACHE"] = "/tmp/data"
+
+app = FastAPI()
+handler = Mangum(app)
+
+
+@app.post('/transcribed')
+def get_text():
+    try:
+        command = [
+            '/usr/share/ffmpeg',
+            'ffmpeg',
+            '-i',
+            video_url,
+            '-b:a', '64k',
+            '-f', 'wav',  # Force output format to WAV
+            'pipe:1'  # Send output to stdout
+        ]
+
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav:
+            temp_wav.write(stdout)
+
+        model = whisper.load_model("base")
+        result = model.transcribe(temp_wav.name, fp16=False)
+        print("Answer:", result["text"])
+        return {
+            "transcribed": result["text"],
+        }
+
+    except Exception as e:
+        print(e)
+        # Handle exceptions and return an appropriate error response
+        return HTTPException(status_code=500, detail=f"Error processing data: {str(e)}")
