448e5da804dc1c9a3faf8dcbf3b2bf515888a52b
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2024-02-28 14:05:08 +0530

    pushing new paths

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM public.ecr.aws/lambda/python:3.11
 
-
-
 COPY . .
 
 # Create a directory for data
@@ -13,14 +11,15 @@ ENV TRANSFORMERS_CACHE "/tmp/data"
 ENV LIBROSA_CACHE_DIR "/tmp"
 ENV NUMBA_CACHE_DIR "/tmp"
 
-COPY ffmpeg '/usr/share/'
+#COPY ffmpeg '/usr/share/'
+#
+#COPY Tool '/tmp'
 
-COPY Tool '/tmp'
-
-
-
-# Copy requirements.txt and install the required packages
-#COPY requirements.txt .
+RUN yum -y install git wget tar xz
+RUN wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz && tar xvf ffmpeg-release-amd64-static.tar.xz && mv ffmpeg-6.0-amd64-static/ffmpeg /usr/bin/ffmpeg && rm -Rf ffmpeg*
+RUN pip install --no-cache-dir setuptools-rust
+RUN pip install --no-cache-dir git+https://github.com/openai/whisper.git
+RUN whisper --model_dir /usr/local --model medium audio >> /dev/null 2>&1; exit 0
 RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
 
 #RUN python -m nltk.downloader punkt
--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import whisper
 import ssl
 import subprocess
-from fastapi import FastAPI,  HTTPException
+from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 import os
 import tempfile
+from pydub import AudioSegment
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 video_url = 'https://d8cele0fjkppb.cloudfront.net/ivs/v1/624618927537/h3DzFIBds0W6/2024/2/21/13/34/9426YpfiMxUD/media/hls/master.m3u8'
+
 
 os.environ["TRANSFORMERS_CACHE"] = "/tmp/data"
 
@@ -20,8 +22,8 @@ handler = Mangum(app)
 def get_text():
     try:
         command = [
-            # '/usr/share/ffmpeg',
-            'ffmpeg',
+            '/usr/share/ffmpeg',
+            # 'ffmpeg',
             '-i',
             video_url,
             '-b:a', '64k',
@@ -62,14 +64,47 @@ def get_transcribing():
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        with tempfile.NamedTemporaryFile(dir='/tmp',delete=False, suffix=".wav") as temp_wav:
+        with tempfile.NamedTemporaryFile(dir='/tmp', delete=False, suffix=".wav") as temp_wav:
             temp_wav.write(stdout)
 
         model = whisper.load_model("base")
-        result = model.transcribe(f"{temp_wav.name}", fp16=False)
+        result = model.transcribe(f"{temp_wav.name}", word_timestamps=True, fp16=False)
         print("Answer:", result["text"])
         return {
-            "transcribed": result["text"],
+            "transcribed": result,
+        }
+
+    except Exception as e:
+        print(e)
+        # Handle exceptions and return an appropriate error response
+        return HTTPException(status_code=500, detail=f"Error processing data: {str(e)}")
+
+
+@app.post('/transcribe')
+def get_transcribe():
+    try:
+        os.chdir('/tmp/data')
+        video_url = "https://d8cele0fjkppb.cloudfront.net/ivs/v1/624618927537/h3DzFIBds0W6/2024/2/21/13/34/9426YpfiMxUD/media/hls/master.m3u8"
+        command = [
+            '/usr/bin/ffmpeg',
+            # '/usr/share/ffmpeg',
+            'ffmpeg',
+            '-i',
+            video_url,
+            '-b:a', '64k',
+            '-f', 'wav',  # Force output format to WAV
+            '/tmp/1.wav'  # Send output to stdout
+        ]
+
+        subprocess.run(command, check=True)
+
+        # Transcribe the 10-second audio segment
+        model = whisper.load_model("base")
+        result = model.transcribe('/tmp/1.wav', fp16=False)
+        print("Answer:", result["text"])
+
+        return {
+            "transcribed": result,
         }
 
     except Exception as e:
